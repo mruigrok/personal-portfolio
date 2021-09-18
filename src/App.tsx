6e3621e09841f44fda940cac5ccd8bc67c08da34
Matt Ruigrok <mruigrok6@gmail.com>
2021-09-18 13:12:35 -0400

    change to serve only homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Routes from './view/Routes';
+// import Routes from './view/Routes';
+import HomePage from './view/Pages/Home';
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function App() {
   return (
     <div className="App">
-      <Routes />
+      <HomePage />
     </div>
   );
 }
