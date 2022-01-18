7a314c0c63db29027e75629ee0b5511b6a81479c
Matt Ruigrok <mruigrok6@gmail.com>
2022-01-18 12:07:19 -0500

    Update domain

--- src/view/Routes/index.tsx
+++ src/view/Routes/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route, HashRouter } from 'react-router-dom';
 import HomePage from '../Pages/Home';
 import ProjectsPage from '../Pages/Projects';
 
 const Routes = (): JSX.Element => {
   return (
     <>
-      {/* <BrowserRouter basename="/personal-portfolio"> */}
-      <BrowserRouter basename="/">
+      <HashRouter basename="/personal-portfolio">
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/projects" component={ProjectsPage} />
           <Route path="/" render={() => <div> 404 </div>} />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 };
